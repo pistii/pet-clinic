c83ff914451adf2cd9679ee8afb4da9b7b7b01ad
pistii <pilisi.istvan@gmail.com>
2025-03-13 13:46:28 +0100

    Add user routes endpoints

--- Project-be/user_routes/user_routes.py
+++ Project-be/user_routes/user_routes.py
@@ -1,40 +1,19 @@
+import os
 from fastapi import APIRouter, HTTPException
-from models import User
-from bson import ObjectId
-
-
-# MongoDB kapcsolat létrehozása
-
-
-db = client["pet_clinic"]
-users_collection = db["user"]
-client = MongoClient("mongodb://localhost:27017")
-db = client["pet_clinic"]
-users_collection = db["users"]
-
-router = APIRouter()
-
-# Új felhasználó létrehozása
-@router.post("/")
-async def create_user(user: User):
-    new_user = await users_collection.insert_one(user.dict())
-    return {"id": str(new_user.inserted_id)}
-
-# Felhasználó lekérése ID alapján
-@router.get("/{user_id}")
-async def get_user(user_id: str):
-    user = await users_collection.find_one({"_id": ObjectId(user_id)})
-    if not user:
-        raise HTTPException(status_code=404, detail="User not found")
-    return user
-
-
+from pymongo import MongoClient
+from dotenv import load_dotenv
 from fastapi import APIRouter, HTTPException
-from models.user import UserCreate, UserResponse
+from models.User import UserResponse, UserCreate
 from repositories.user_repository import UserRepository
 
-router = APIRouter(prefix="/users", tags=["Users"])
+load_dotenv()
+MONGO_URI = os.getenv("MONGO_URI")
 
+client = MongoClient(MONGO_URI)
+db = client["pet_clinic"]
+users_collection = db["user"]
+
+router = APIRouter(prefix="/users", tags=["Users"])
 
 @router.post("/", response_model=UserResponse)
 def create_user(user: UserCreate):
@@ -52,3 +31,4 @@ def get_user(user_id: str):
     if not user:
         raise HTTPException(status_code=404, detail="User not found")
     return user
+
