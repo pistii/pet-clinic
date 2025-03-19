4d239c92f349259151fd6f3769132919fcf4f96e
pistii <pilisi.istvan@gmail.com>
2025-03-19 15:11:08 +0100

    Update pet models

--- Project-be/models/Pet.py
+++ Project-be/models/Pet.py
@@ -1,3 +1,4 @@
+import uuid
 from pydantic import BaseModel, Field
 from typing import Optional
 from datetime import datetime
@@ -13,12 +14,12 @@ class Pet(BaseModel):
 
 
 class PetCreate(BaseModel):
-    pet_id: Optional[str] = Field()
-    species: Optional[str] = Field()
-    breed: Optional[str] = Field()
+    pet_id: str = Field(default_factory=lambda: str(uuid.uuid4()))
+    species: Optional[str] = Field(None)
+    breed: Optional[str] = Field(None)
     name: str = Field(...)
     sex: str = Field(..., pattern="^(male|female)$")
-    date_of_birth: datetime = Field()
+    date_of_birth: datetime = Field(None)
 
 
 class PetResponse(BaseModel):
