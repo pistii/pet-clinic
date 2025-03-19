43815094a2a519a9d253167a6d1efba9558236d2
pistii <pilisi.istvan@gmail.com>
2025-03-19 15:28:08 +0100

    separate into buisness logic and data access layer

--- Project-be/routes/user_routes.py
+++ Project-be/routes/user_routes.py
@@ -1,25 +1,16 @@
-import os
-from dotenv import load_dotenv
 from typing import Annotated
-from pymongo import MongoClient
 from fastapi import APIRouter, Depends, HTTPException
 from fastapi.security import OAuth2PasswordRequestForm
-from fastapi import APIRouter, HTTPException
 
 from models.User import User, UserResponse, UserCreate
 from models.token import Token
 
-from repositories.token import add_refresh_token, remove_refresh_token, get_refresh_token, update_refresh_token
+from repositories.token import add_refresh_token, update_refresh_token
 from repositories.user_repository import UserRepository
-from auth.auth import authenticate_user, create_token_pair, validate_refresh_token
+
+from auth.auth import authenticate_user, create_token_pair, get_current_user, validate_refresh_token
 from auth.RoleChecker import RoleChecker
 
-load_dotenv()
-MONGO_URI = os.getenv("MONGO_URI")
-
-client = MongoClient(MONGO_URI)
-db = client["pet_clinic"]
-users_collection = db["users"]
 
 router = APIRouter(prefix="/api/users", tags=["Users"])
 
@@ -49,7 +40,7 @@ def create_admin(user: UserCreate):
 
 
 #Receives the user with the given id
-@router.get("/{user_id}", response_model=UserResponse)
+@router.get("/get_by_id/{user_id}", response_model=UserResponse)
 def get_user(user_id: str):
     user = UserRepository.get_user_by_id(user_id)
     if not user:
@@ -57,17 +48,11 @@ def get_user(user_id: str):
     return user
 
 
-  
-@router.get("/data")  
-def get_data(_: Annotated[bool, Depends(RoleChecker(required_role=["admin"]))]):  
-  return {"data": "This is important data"}
-
 @router.post("/login")  
 async def login_for_access_token(form_data: Annotated[OAuth2PasswordRequestForm, Depends()]) -> Token:
-    user = authenticate_user(form_data.username, form_data.password)  
+    user = await authenticate_user(form_data.username, form_data.password)  
     if not user:  
         raise HTTPException(status_code=400, detail="Incorrect username or password")  
-      
      
     access_token, refresh_token = create_token_pair(user)
     add_refresh_token(refresh_token)
@@ -81,5 +66,6 @@ async def refresh_access_token(token_data: Annotated[tuple[User, str], Depends(v
     update_refresh_token(refresh_token, token)
 
     return Token(access_token=access_token, refresh_token=refresh_token)
+
 
 
