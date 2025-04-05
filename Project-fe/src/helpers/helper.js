0a19d5a2f264bac0858b0cb0a4420a97a473253a
pistii <pilisi.istvan@gmail.com>
2025-04-05 14:16:04 +0200

    helper functions for datepicker and form data

--- Project-fe/src/helpers/helper.js
+++ Project-fe/src/helpers/helper.js
@@ -12,3 +12,24 @@ export function formatDate(date_param)
     let date_format =  yyyy + '-' + mm + '-' + dd + '<br>' + hh + ":" + min;
     return date_format;
 }
+
+
+export function datePickerFormat(date_param) {
+    let date = new Date(date_param);
+    let dd = String(date.getDate()).padStart(2, '0');
+    let mm = String(date.getMonth() + 1).padStart(2, '0');
+    let yyyy = date.getFullYear();
+    let date_format =  yyyy + '-' + mm + '-' + dd;
+    if (yyyy == 1970) return null;
+    return date_format;
+}
+
+export function bindFormData(formId, data) {
+    const form = document.getElementById(formId);
+    
+    Object.keys(data).forEach(key => {
+        if (form[key]) {
+            form[key].value = data[key];
+        }
+    });
+}
